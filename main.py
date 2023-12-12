2c13208ec6a7d3ec63c92cf8c022f357eb1c3e89
lefkats94 <lefkats94@gmail.com>
2023-12-12 13:41:24 +0200

    final version

--- main.py
+++ main.py
@@ -1,42 +1,167 @@
-from read_files import read_sentinel2_bands
-from help_methods import clean_outliers, calculate_ndvi, calculate_rescaled_array, moving_average_smooth
-import conv_peakdet
+from methods import (
+    get_metadata,
+    clean_outliers,
+    peakdet,
+    moving_average_smooth,
+    read_sentinel2_bands
+)
 import numpy as np
+from osgeo import gdal
+from PIL import Image
+
+class WaterMaskClassifier:
+    def __init__(self, bands_directory):
+        self.bands_directory = bands_directory.replace("\\", "/")
+        if not self.bands_directory.endswith("/"):
+            self.bands_directory += "/"
+        self.red_array, self.green_array, self.nir_array, self.red_edge1_array, self.swir_array, self.swir2_array = self.preprocess()
+
+    def preprocess(self):
+        """
+        Read Sentinel-2 bands and preprocess SWIR arrays by cleaning outliers.
+
+        Returns:
+        Tuple of numpy arrays: (red_array, green_array, nir_array, red_edge1_array, swir_array, swir2_array)
+        """
+        red_array, green_array, nir_array, red_edge1_array, swir_array, swir2_array = read_sentinel2_bands(self.bands_directory)
+        swir_array = clean_outliers(swir_array)
+        swir2_array = clean_outliers(swir2_array)
+        return red_array, green_array, nir_array, red_edge1_array, swir_array, swir2_array
+
+    def swir_thresholds(self):
+        """
+        Detects and returns the positions of the first and second deep valleys in a Short-Wave Infrared (SWIR) array.
+
+        Returns:
+        Tuple of two values: (first_deep_valley, second_deep_valley)
+        """
+        swir_flat = self.swir_array.flatten()
+        swir_hist, bin_edges = np.histogram(swir_flat[~np.isnan(swir_flat)], bins=255)
+        swir_hist_smoothed = moving_average_smooth(swir_hist)
+        swir_smoothed_peaks, swir_smoothed_valleys = peakdet(swir_hist_smoothed, np.mean(swir_hist_smoothed)/3)
+        swir_valleys_smoothed_only = [x[0] for x in swir_smoothed_valleys]
+        first_swir_threshold = np.amax(self.swir_array)*(swir_valleys_smoothed_only[0]/255) + np.amin(self.swir_array)
+        second_swir_threshold = np.amax(self.swir_array)*(swir_valleys_smoothed_only[1]/255) + np.amin(self.swir_array)
+        return first_swir_threshold, second_swir_threshold
+
+    def ndvi_threshold(self):
+        """
+        Computes and returns the Normalized Difference Vegetation Index (NDVI) threshold from NIR and Red bands.
+
+        Returns:
+        Tuple of two values: (ndvi_threshold, ndvi)
+        """
+        ndvi = np.divide(self.nir_array - self.red_array, self.nir_array + self.red_array, out=np.ones_like(self.nir_array) * -2, where=(self.nir_array + self.red_array) != 0)
+        ndvi_flattened = ndvi.flatten()
+        ndvi_hist, _ = np.histogram(ndvi_flattened[~np.isnan(ndvi_flattened)], bins=201, range=[-1.0, 1.0])
+        ndvi_peaks, ndvi_valleys = peakdet(ndvi_hist, np.mean(ndvi_hist)/10)
+        ndvi_valleys_only = [x[0] for x in ndvi_valleys]
+        ndvi_valleys_only_ranged = [(x - 100)/100.0 for x in ndvi_valleys_only]
+
+        if len(ndvi_valleys_only_ranged) > 0:
+            for ndvi_threshold in (x for x in ndvi_valleys_only_ranged if x > 0.3):
+                break
+        else:
+            ndvi_threshold = -1
+
+        return ndvi_threshold, ndvi
+
+    def floating_vegetation(self):
+        """
+        Computes and returns a binary mask indicating floating vegetation areas.
+
+        Returns:
+        numpy.ndarray: Binary mask indicating floating vegetation areas.
+        """
+        floating_index = np.divide(self.red_edge1_array, self.swir_array, out=np.ones_like(self.swir_array) * -2, where=(self.red_edge1_array + self.swir_array) != 0)
+        ndwi = np.divide(self.nir_array - self.swir_array, self.nir_array + self.swir_array, out=np.ones_like(self.green_array) * -2, where=(self.green_array + self.nir_array) != 0)
+        floating_mask = np.full(floating_index.shape, False)
+
+        condition_mask = (0.6 < floating_index) & (floating_index < 1.5) & (0.2 < ndwi) & (ndwi < 0.45) & (100 < self.swir2_array) & (self.swir2_array < 900)
+        floating_mask[condition_mask] = True
+        return floating_mask
+
+    def watermask_calculation(self, ndvi_thres, floating_mask):
+        """
+        Computes and returns a classified water mask.
+
+        Parameters:
+        - ndvi_thres (float): NDVI threshold.
+        - floating_mask (numpy.ndarray): Binary mask indicating floating vegetation areas.
+
+        Returns:
+        numpy.ndarray: Classified water mask.
+        """
+        first_swir_threshold, second_swir_threshold = self.swir_thresholds()
+        classified_area = np.full(floating_mask.shape, 0)
+        classified_area[self.swir_array < first_swir_threshold] = 1
+        classified_area[(self.swir_array >= first_swir_threshold) & (self.swir_array <= second_swir_threshold) & (self.ndvi > ndvi_thres)] = 2
+        classified_area[floating_mask] = 3
+        return classified_area
+
+    def create_outputs(self):
+        """
+        Creates output files including a GeoTIFF and a PNG image for the classified water mask.
+        """
+        inputs_directory = self.bands_directory
+        inputs_directory = inputs_directory.replace("\\", "/")
+        if not inputs_directory.endswith("/"):
+            inputs_directory += "/"
+
+        x, y = self.classified_area.shape
+
+        driver = gdal.GetDriverByName('GTiff')
+        dataset = driver.Create(inputs_directory + "classification.tif", y, x, 1, gdal.GDT_Float32)
+        dataset.GetRasterBand(1).WriteArray(self.classified_area)
+        dataset.GetRasterBand(1).SetNoDataValue(-1)
+
+        random_band = get_metadata(inputs_directory)
+
+        geotrans = random_band.GetGeoTransform()
+        proj = random_band.GetProjection()
+        dataset.SetGeoTransform(geotrans)
+        dataset.SetProjection(proj)
+
+        dataset = None
+
+        rgb = np.zeros((x, y, 3))
+
+        rgb[:,:,0] = np.where((self.classified_area == 0), 255, rgb[:,:,0])
+        rgb[:,:,1] = np.where((self.classified_area == 0), 128, rgb[:,:,1])
+        rgb[:,:,2] = np.where((self.classified_area == 0), 0, rgb[:,:,2])
+
+        rgb[:,:,0] = np.where((self.classified_area == 1), 0, rgb[:,:,0])
+        rgb[:,:,1] = np.where((self.classified_area == 1), 0, rgb[:,:,1])
+        rgb[:,:,2] = np.where((self.classified_area == 1), 255, rgb[:,:,2])
+
+        rgb[:,:,0] = np.where((self.classified_area == 2), 0, rgb[:,:,0])
+        rgb[:,:,1] = np.where((self.classified_area == 2), 102, rgb[:,:,1])
+        rgb[:,:,2] = np.where((self.classified_area == 2), 0, rgb[:,:,2])
+
+        rgb[:,:,0] = np.where((self.classified_area == 3), 0, rgb[:,:,0])
+        rgb[:,:,1] = np.where((self.classified_area == 3), 255, rgb[:,:,1])
+        rgb[:,:,2] = np.where((self.classified_area == 3), 0, rgb[:,:,2])
+
+        im = Image.fromarray(rgb.astype("uint8"))
+        im.save(inputs_directory + "classification.png")
 
 if __name__ == "__main__":
-    # call the read_ndvi_data function
-    bands_directory = input("Enter the NDVIS directory:")
+    print("""
+    Sentinel-2 Bands Required:
+    --------------------------
 
-    # correct the given directory
-    bands_directory = bands_directory.replace("\\", "/")
-    if not bands_directory.endswith("/"):
-        bands_directory += "/"
-    
-    # read the necessary Sentinel-2 bands
-    blue_array, red_array, green_array, nir_array, red_edge1_array, red_edge3_array, swir_array, swir2_array, narrow_nir_array = read_sentinel2_bands("C:/Users/lefkats_local/Desktop/test_watermask/")
+    - B03 (Green Band, 10m resolution)
+    - B04 (Red Band, 10m resolution)
+    - B08 (NIR Band, 10m resolution)
+    - B05 (Red Edge 1 Band, 20m resolution)
+    - B11 (SWIR Band, 20m resolution)
+    - B12 (SWIR 2 Band, 20m resolution)
 
-    # clean the outliers of the swir bands
-    swir_array = clean_outliers(swir_array)
-    swir2_array = clean_outliers(swir2_array)
-
-    #rescale the swir band
-    swir_array = calculate_rescaled_array(swir_array)
-
-    # flat the swir band
-    swir_flat = swir_array.flatten()
-
-    # calculate the swir histogram
-    swir_hist, bin_edges = np.histogram(swir_flat[~np.isnan(swir_flat)], bins = 255)
-
-    # identify the peaks and valleys in the swir's histogram
-    swir_peaks, swir_valleys = conv_peakdet.peakdet(swir_hist, np.mean(swir_hist)/3)
-
-    # smooth the histogram
-    swir_hist_smoothed = moving_average_smooth(swir_hist)
-
-    # identify the peaks and valleys in the swir's smoothed histogram
-    swir_smoothed_peaks, swir_smoothed_valleys = conv_peakdet.peakdet(swir_hist_smoothed, np.mean(swir_hist_smoothed)/3)
-
-    # calculate the ndvi
-    ndvi_array = calculate_ndvi(red_array, nir_array)
-    print(swir_smoothed_valleys)
+    Note: Ensure these Sentinel-2 bands (in .tif format) are available in the specified input directory.
+    """)
+    inputs_dir = input("insert the inputs directory:")
+    water_mask_classifier = WaterMaskClassifier(inputs_dir)
+    ndvi_thres, water_mask_classifier.ndvi = water_mask_classifier.ndvi_threshold()
+    water_mask_classifier.floating_mask = water_mask_classifier.floating_vegetation()
+    water_mask_classifier.classified_area = water_mask_classifier.watermask_calculation(ndvi_thres, water_mask_classifier.floating_mask)
+    water_mask_classifier.create_outputs()
---
+++ methods.py
@@ -0,0 +1,185 @@
+import numpy as np
+from scipy.signal import lfilter
+from osgeo import gdal
+
+def get_metadata(inputs_directory):
+    """
+    Retrieves metadata from a Sentinel-2 band.
+
+    Parameters:
+    - inputs_directory (str): The directory containing the Sentinel-2 bands.
+
+    Returns:
+    GDAL Dataset: Metadata information for the specified band.
+    """
+    random_band = gdal.Open(inputs_directory + "B04.tif")
+    return random_band
+
+def read_sentinel2_bands(inputs_directory):
+    """
+    Reads and returns Sentinel-2 bands.
+
+    Parameters:
+    - inputs_directory (str): The directory containing the Sentinel-2 bands.
+
+    Returns:
+    Tuple of numpy arrays: (red_array, green_array, nir_array, red_edge1_array, swir_array, swir2_array)
+    """
+    # 10m resolution bands
+    green = gdal.Open(inputs_directory + "B03.tif")
+    srcband = green.GetRasterBand(1)
+    green_array = srcband.ReadAsArray(0, 0, green.RasterXSize, green.RasterYSize).astype(np.float)
+
+    red = gdal.Open(inputs_directory + "B04.tif")
+    srcband = red.GetRasterBand(1)
+    red_array = srcband.ReadAsArray(0, 0, red.RasterXSize, red.RasterYSize).astype(np.float)
+
+    nir = gdal.Open(inputs_directory + "B08.tif")
+    srcband = nir.GetRasterBand(1)
+    nir_array = srcband.ReadAsArray(0, 0, nir.RasterXSize, nir.RasterYSize).astype(np.float)
+
+    # 20m resolution bands
+    red_edge1 = gdal.Open(inputs_directory + "B05.tif")
+    srcband = red_edge1.GetRasterBand(1)
+    red_edge1_array = srcband.ReadAsArray(0, 0, red_edge1.RasterXSize, red_edge1.RasterYSize, nir.RasterXSize, nir.RasterYSize).astype(np.float)
+
+    swir = gdal.Open(inputs_directory + "B11.tif")
+    srcband = swir.GetRasterBand(1)
+    swir_array = srcband.ReadAsArray(0, 0, swir.RasterXSize, swir.RasterYSize, nir.RasterXSize, nir.RasterYSize).astype(np.float)
+
+    swir2 = gdal.Open(inputs_directory + "B12.tif")
+    srcband = swir2.GetRasterBand(1)
+    swir2_array = srcband.ReadAsArray(0, 0, swir2.RasterXSize, swir2.RasterYSize, nir.RasterXSize, nir.RasterYSize).astype(np.float)
+
+    return red_array, green_array, nir_array, red_edge1_array, swir_array, swir2_array
+
+def clean_outliers(band_array):
+    """
+    Cleans outliers in the input band array.
+
+    Parameters:
+    - band_array (numpy.ndarray): Input band array.
+
+    Returns:
+    numpy.ndarray: Cleaned band array.
+    """
+    band_array[band_array < -1000000] = 0
+    band_array[band_array > 1000000] = 0
+    band_array[np.isnan(band_array)] = 0
+    band_array[np.isinf(band_array)] = 0
+    return band_array
+
+def calculate_rescaled_array(band_array):
+    """
+    Calculates and returns a rescaled version of the input band array.
+
+    Parameters:
+    - band_array (numpy.ndarray): Input band array.
+
+    Returns:
+    numpy.ndarray: Rescaled band array.
+    """
+    rescaled_array = (band_array - np.amin(band_array)) / np.amax(band_array)
+    return rescaled_array
+
+def moving_average_smooth(Y, width=5):
+    """
+    Applies a moving average smoothing to the input array.
+
+    Parameters:
+    - Y (numpy.ndarray): Input array.
+    - width (int): Width of the moving average window.
+
+    Returns:
+    numpy.ndarray: Smoothed array.
+    """
+    n = len(Y)
+    c = lfilter(np.ones(width)/width, 1, Y)
+    cbegin = np.cumsum(Y[0:width-2])
+    cbegin_div = np.arange(1, (width-1), 2).astype(float)
+    cbegin = cbegin[::2] / cbegin_div
+    cend = np.cumsum(Y[(n-1):(n-width+3-2):-1])
+    cend_div = np.arange(width-2, 0, -2).astype(float)
+    cend = cend[(n)::-2] / cend_div
+    c = np.concatenate((cbegin, c[(width-1)::], cend))
+    return c
+
+
+def peakdet(v=None, delta=None, x=None):
+    
+    '''
+    PEAKDET Detect peaks in a vector
+            [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+            maxima and minima ("peaks") in the vector V.
+            MAXTAB and MINTAB consists of two columns. Column 1
+            contains indices in V, and column 2 the found values.
+          
+            With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+            in MAXTAB and MINTAB are replaced with the corresponding
+            X-values.
+    
+            A point is considered a maximum peak if it has the maximal
+            value, and was preceded (to the left) by a value lower by
+            DELTA.
+    '''
+    maxtab = []
+    mintab = []
+
+    v = np.array(v)
+       
+    #if args < 3 actually
+    if x is None:
+        x = range(0, len(v))
+        x = np.array(x)
+    else:
+        if len(v) != len(x):
+            print('Input vectors v and x must have same length')
+            quit()
+    delta = np.array(delta)
+    
+    if (delta.size)>1:
+        print('Input argument DELTA must be a scalar')
+        quit()
+    if delta <= 0:
+        print("Input argument DELTA must be positive")
+        quit()
+    
+    mn = float('Inf')
+    mx = -float('Inf')
+
+    mnpos = float('NaN')
+    mxpos = float('NaN')
+
+    lookformax = 1
+
+    for i in range (1,len(v)+1):
+      temp = v[i-1]
+      if temp > mx:
+        mx = temp
+        mxpos = x[i-1]
+      if temp < mn:
+        mn = temp
+        mnpos = x[i-1]
+
+      
+      if lookformax:
+        if temp < mx - delta:
+            temp_maxtab = []
+            temp_maxtab.append(mxpos)
+            temp_maxtab.append(mx)
+            maxtab.append(temp_maxtab)
+            mn = temp
+            mnpos = x[i-1]
+            lookformax = 0
+          
+      else:
+          if temp > mn + delta:
+            temp_mintab = []
+            temp_mintab.append(mnpos)
+            temp_mintab.append(mn)
+            mintab.append(temp_mintab)  
+            mx = temp
+            mxpos = x[i-1]
+            lookformax = 1
+
+    return maxtab, mintab
